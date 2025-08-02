881c5b611663b80c995284a215b4c642e4650c45
Cozy-Habit <sophia.precker77@gmail.com>
2025-08-02 21:03:56 +0200

    feat: add route and config file namespaces

---
+++ src/views/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <HomeLayout>
+    <h1>You did it!</h1>
+    <p>
+      Visit
+      <a
+        href="https://vuejs.org/"
+        target="_blank"
+        rel="noopener"
+        >vuejs.org
+      </a>
+      to read the documentation
+    </p>
+  </HomeLayout>
+</template>
+
+<script setup lang="ts">
+import { HomeLayout } from '@layouts';
+</script>
+
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@layouts': fileURLToPath(
+        new URL('./src/layouts/index.ts', import.meta.url)
+      ),
+      '@components': fileURLToPath(
+        new URL('./src/components/index.ts', import.meta.url)
+      ),
+      '@assets': fileURLToPath(
+        new URL('./src/assets/index.ts', import.meta.url)
+      ),
+      '@views': fileURLToPath(new URL('./src/views/index.ts', import.meta.url)),
+      '@stores': fileURLToPath(
+        new URL('./src/stores/index.ts', import.meta.url)
+      ),
     },
   },
-})
+});
