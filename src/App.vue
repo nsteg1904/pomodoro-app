25c71da38d9aeee7eda8ae6e135b1425f588877c
Cozy-Habit <sophia.precker77@gmail.com>
2025-08-02 20:27:40 +0200

    feat: add HomeLayout component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a
-      href="https://vuejs.org/"
-      target="_blank"
-      rel="noopener"
-      >vuejs.org
-    </a>
-    to read the documentation
-  </p>
+  <HomeLayout>
+    <h1>You did it!</h1>
+    <p>
+      Visit
+      <a
+        href="https://vuejs.org/"
+        target="_blank"
+        rel="noopener"
+        >vuejs.org
+      </a>
+      to read the documentation
+    </p>
+  </HomeLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HomeLayout from './layouts/HomeLayout.vue';
+</script>
 
 <style scoped></style>
---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="$style.homeLayout">
+    <header :class="$style.homeLayout__header">Header</header>
+    <main :class="$style.homeLayout__main"><slot /></main>
+    <aside :class="$style.homeLayout__aside">
+      <div>Left Action Bar</div>
+      <div>Right Action Bar</div>
+    </aside>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style module lang="scss">
+.homeLayout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  &__main {
+    margin: auto;
+  }
+
+  &__aside {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+}
+</style>
